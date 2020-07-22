12ce763aa3e28be5e43e000ed75785509809c171
Wilson Cordeiro <wilson.cordeiro@accenture.com>
2020-07-22 16:10:33 -0300

    remove the bodyparser

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const db = require('./queries');
 const app = express();
 const cors = require('cors');
 const port = 3000;
 
-app.use(bodyParser.json());
 app.use(cors());
-app.use(
-    bodyParser.urlencoded({
-        extended: true
-    })
-);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 /* Create an route to look on the root and return a ok message */
 app.get('/', (request, response) => {
